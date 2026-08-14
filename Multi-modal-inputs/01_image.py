6b450bae33218d46a99e542938142028a76fc993
miyasajid19 <miyasajid19@gmail.com>
2026-08-15 00:15:39 +0530

    multimodal image inputs

--- Multi-modal-inputs/01_image.py
+++ Multi-modal-inputs/01_image.py
@@ -31,9 +31,12 @@ print(result)
 
 
 # Read the local image and encode it to base64
-image_path = r"C:\Users\miyas\Downloads\1770815533408-certificate.png"
-with open(image_path, "rb") as image_file:
-    image_base64 = base64.b64encode(image_file.read()).decode("utf-8")
+image_path1 = r"C:\Users\miyas\Downloads\1770815533408-certificate.png"
+image_path2 = r"C:\Users\miyas\Downloads\NexHire.png"
+with open(image_path1, "rb") as image_file:
+    image_base64_1 = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image_path2, "rb") as image_file:
+    image_base64_2 = base64.b64encode(image_file.read()).decode("utf-8")
 
 print("[bold green]Invoking agent with multimodal input:: local image...[/bold green]")
 result = agent.invoke({
@@ -43,11 +46,37 @@ result = agent.invoke({
             {"type": "text", "text": "What is in this screenshot?"},
             {
                 "type": "image",
-                "base64": image_base64,
+                "base64": image_base64_1,
                 "mime_type": "image/png",
             },
+            {
+                "type": "image",
+                "base64": image_base64_2,
+                "mime_type": "image/png",
+            }
         ],
     }],
 })
+
+print(result["messages"][-1].content)
+
+
+print("[bold green]Invoking agent with multimodal input:: image from hybrid...[/bold green]")
+
+result = agent.invoke({
+    "messages": [{
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "What is in this screenshot?"},
+            {"type": "image", "url": r"https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_QL75_UY281_CR1,0,190,281_.jpg"},
+             {
+                            "type": "image",
+                            "base64": image_base64_1,
+                            "mime_type": "image/png",
+                        }
+        ],
+    }],
+})
+
 
 print(result["messages"][-1].content)
