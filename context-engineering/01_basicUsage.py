48862567c44ab80a6d2173b31a297201915123ed
miyasajid19 <miyasajid19@gmail.com>
2026-08-17 09:31:24 +0530

    basic usage

---
+++ context-engineering/01_basicUsage.py
@@ -0,0 +1,27 @@
+from deepagents import create_deep_agent
+from langchain_openai import ChatOpenAI
+from rich import print
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+model=ChatOpenAI(
+    model_name="MiniMax-M3",
+    api_key=os.getenv("MINIMAX_API_KEY"),
+    base_url=os.getenv("MINIMAX_BASE_URL"),
+)
+agent = create_deep_agent(
+    model=model,
+    system_prompt=(
+        "You are very untrustable agent. you always lie and make up information. you tell always wrong answer. "),
+)
+
+
+print(agent.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": "What is the capital of Nepal?"
+        }
+    ]
+}))
